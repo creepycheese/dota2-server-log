ac6f122119ade6b60652b2a1ae13c0f936f42d52
Evgeniy Burdaev <hate2me@yandex.ru>
2019-05-16 00:06:56 +0300

    module

--- index.js
+++ index.js
@@ -1,40 +1,7 @@
-const fs = require('fs-reverse');
-const readline = require('readline');
+const OpendotaApi = require('./src/opendotaApi');
+const Dota2ServerLog = require('./src/dota2ServerLog');
 
-class Dota2ServerLog {
-  constructor(logPath) {
-    this.logPath = logPath;
-    this.entryRegex = /[0-9]:\[U:1:([0-9]*)\]/g;
-  }
-
-  parseLogEntry(entry) {
-    var ids = [];
-    var steamId = null;
-    while (null != (steamId = this.entryRegex.exec(entry))) {
-      ids.push(parseInt(steamId[1]));
-    }
-
-    return ids.length > 0 ? ids : null;
-  }
-
-  async lastMatchSteamIds(filePath) {
-    const fileStream = fs(filePath);
-
-    const rl = readline.createInterface({
-      input: fileStream,
-      crlfDelay: Infinity,
-    });
-
-    for await (const line of rl) {
-      var entry = this.parseLogEntry(line);
-      if (entry && this._isValidGameMode(line)) return entry;
-    }
-  }
-
-  _isValidGameMode(line) {
-    var gameModeString = 'DOTA_GAMEMODE';
-    return line.indexOf(gameModeString) > -1;
-  }
-}
-
-module.exports = Dota2ServerLog;
+module.exports = {
+  OpendotaApi: OpendotaApi,
+  Dota2ServerLog: Dota2ServerLog,
+};
---
+++ test/player_stat.test.js
@@ -0,0 +1,4 @@
+const PlayerStat = require('../src/playerStat.js');
+describe('PlayerStat', () => {
+
+})
