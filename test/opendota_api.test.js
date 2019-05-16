17e354d2bc7a6799200f61d03f41e9be4d022026
Evgeniy Burdaev <hate2me@yandex.ru>
2019-05-16 13:49:15 +0300

    Refactor specs to remove duplication

--- test/opendota_api.test.js
+++ test/opendota_api.test.js
@@ -1,19 +1,24 @@
 let OpendotaApi = require('../src/opendotaApi.js');
 const nock = require('nock');
 
+function stubOpendotaGetRequest(path, response, status) {
+  var status = status ? status : 200;
+  nock('https://api.opendota.com/api')
+    .get(path)
+    .reply(status, response);
+}
+
 describe('OpendotaApi', () => {
   let opendota = new OpendotaApi();
 
   describe('heroes', () => {
     let requestUrl =
-      '/910673288/heroes?date=100&limit=20&significant=1&having=2';
+      '/players/910673288/heroes?date=100&limit=20&significant=1&having=2';
     let response = require('./response_stubs/heroes_stat.js');
 
     it('requests heroes stat from opendota with given params', async () => {
       expect.assertions(1);
-      nock('https://api.opendota.com/api/players')
-        .get(requestUrl)
-        .reply(200, response);
+      stubOpendotaGetRequest(requestUrl, response);
 
       var data = await opendota.heroes({
         playerId: 910673288,
@@ -26,15 +31,19 @@ describe('OpendotaApi', () => {
     });
   });
 
+  describe('recentMathes', () => {
+    let requestUrl = '/players/910673288/recentMathes?limit=5';
+    let response = require('./response_stubs/recent_matches.js');
+    it('fetches opendota recentMatches', async () => {});
+  });
+
   describe('win_loss', () => {
     let requestUrl = '/players/910673288/wl?limit=5';
     let response = {win: 5, lose: 5};
 
     it('fetches wl opendota endpoint', async () => {
       expect.assertions(1);
-      nock('https://api.opendota.com/api')
-        .get(requestUrl)
-        .reply(200, response);
+      stubOpendotaGetRequest(requestUrl, response);
 
       var data = await opendota.winLose({playerId: 910673288, limit: 5});
       expect(data).toStrictEqual(response);
@@ -47,9 +56,7 @@ describe('OpendotaApi', () => {
 
     it('requests player data with given params', async () => {
       expect.assertions(1);
-      nock('https://api.opendota.com/api')
-        .get(requestUrl)
-        .reply(200, response);
+      stubOpendotaGetRequest(requestUrl, response);
 
       var id = 910673288;
       var data = await opendota.player(id);
