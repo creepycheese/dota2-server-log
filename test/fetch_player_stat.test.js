a3af12582ed0baf5366f66dcceb0b8678d90c654
Evgeniy Burdaev <hate2me@yandex.ru>
2019-05-16 20:23:15 +0300

    Mock test

--- test/fetch_player_stat.test.js
+++ test/fetch_player_stat.test.js
@@ -1,37 +1,49 @@
-const FetchPlayerStat = require('../src/FetchPlayerStat');
+const FetchPlayerStat = require('../src/FetchPlayerStat').FetchPlayerStat;
+let OpendotaApi = require('../src/opendotaApi.js');
+jest.mock('../src/opendotaApi');
 
 describe('FetchPlayerStat', () => {
   let playerId = '910673288';
 
-  var heroesResponse = 'heroes';
-  var playerResponse = 'player';
-  var winLoseResponse = {win: 22, lose: 32};
-  var recentMatchesResponse = ['1', '2'];
-
-  var apiStub = {
-    heroes: jest.fn(),
-    winLose: jest.fn(),
-    recentMatches: jest.fn(),
-    player: jest.fn(),
-  };
-
-  var statStub = jest.fn();
-
-  afterEach(() => {
-    jest.resetAllMocks();
-  });
-
-  beforeEach(() => {
-    apiStub.heroes.mockResolvedValue(heroesResponse);
-    apiStub.recentMatches.mockResolvedValue(recentMatchesResponse);
-    apiStub.player.mockResolvedValue(playerResponse);
-    apiStub.winLose.mockResolvedValue(winLoseResponse);
-  });
-
   describe('with default deps', () => {
+    it('calls OpendotaApi', () => {
+      FetchPlayerStat(123);
+      const mockOpendotaApi = OpendotaApi.mock.instances[0];
+
+      expect(OpendotaApi).toHaveBeenCalledTimes(1);
+      expect(mockOpendotaApi.recentMatches).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('with injected deps', () => {
     const limit = '20';
     const hasStreakGames = 3;
     const forLastDays = '100';
+
+    var heroesResponse = 'heroes';
+    var playerResponse = 'player';
+    var winLoseResponse = {win: 22, lose: 32};
+    var recentMatchesResponse = ['1', '2'];
+
+    var apiStub = {
+      heroes: jest.fn(),
+      winLose: jest.fn(),
+      recentMatches: jest.fn(),
+      player: jest.fn(),
+    };
+
+    var statStub = jest.fn();
+
+    afterEach(() => {
+      jest.resetAllMocks();
+    });
+
+    beforeEach(() => {
+      apiStub.heroes.mockResolvedValue(heroesResponse);
+      apiStub.recentMatches.mockResolvedValue(recentMatchesResponse);
+      apiStub.player.mockResolvedValue(playerResponse);
+      apiStub.winLose.mockResolvedValue(winLoseResponse);
+    });
 
     it('requests api recentMatches', async () => {
       await FetchPlayerStat(playerId, {
@@ -42,7 +54,10 @@ describe('FetchPlayerStat', () => {
         statConstructor: statStub,
       });
 
-      expect(apiStub.recentMatches).toBeCalledWith({playerId: playerId, limit: limit});
+      expect(apiStub.recentMatches).toBeCalledWith({
+        playerId: playerId,
+        limit: limit,
+      });
     });
 
     it('requests api player', async () => {
@@ -88,7 +103,7 @@ describe('FetchPlayerStat', () => {
     });
 
     it('returns result of PlayerStatFunction', async () => {
-     statStub.mockResolvedValue(42);
+      statStub.mockResolvedValue(42);
       var result = await FetchPlayerStat(playerId, {
         limit: limit,
         hasStreakGames: hasStreakGames,
@@ -97,7 +112,7 @@ describe('FetchPlayerStat', () => {
         statConstructor: statStub,
       });
 
-     expect(result).toBe(42);
+      expect(result).toBe(42);
     });
 
     it('requests api winLose', async () => {
