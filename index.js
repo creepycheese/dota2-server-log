8adf4a73aa6c670af36bc59482165e7197896021
Evgeniy Burdaev <hate2me@yandex.ru>
2019-05-17 18:23:45 +0300

    Imports

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const OpendotaApi = require('./src/opendotaApi');
 const Dota2ServerLog = require('./src/dota2ServerLog');
 const FetchPlayerStat = require('./src/fetchPlayerStat').FetchPlayerStat;
+const ServerLogPlayerData = require('./src/serverLogPlayerData.js');
 
 module.exports = {
   OpendotaApi: OpendotaApi,
   Dota2ServerLog: Dota2ServerLog,
   FetchPlayerStat: FetchPlayerStat,
+ServerLogPlayerData: ServerLogPlayerData
 };
