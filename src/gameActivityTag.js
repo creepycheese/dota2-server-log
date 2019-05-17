303a13a047ef7abb00d12198709d217561d8ede9
Evgeniy Burdaev <hate2me@yandex.ru>
2019-05-17 07:34:49 +0300

    Implemented rusty tag

--- src/gameActivityTag.js
+++ src/gameActivityTag.js
@@ -1,11 +1,32 @@
+const OpendotaApi = require('./opendotaApi');
+
 class GameActivityTag {
   constructor(name, condition) {
     this.name = name;
     this.condition = condition;
   }
 
+  static predefinedTags() {
+    return [
+      new GameActivityTag('Rusty', async function(p) {
+        var api = new OpendotaApi();
+        var winLose = await api.winLose({
+          date: 14,
+          playerId: p.player.profile.account_id,
+        });
+        var result = winLose.win + winLose.lose <= 3;
+
+        return result;
+      }),
+    ];
+  }
+
+  static getPredefinedTag(name) {
+    return GameActivityTag.predefinedTags().find(p => p.name == name);
+  }
+
   isSatisfied(playerStat) {
-    this.condition(playerStat);
+    return this.condition(playerStat);
   }
 }
 
--- test/game_activity_tag.test.js
+++ test/game_activity_tag.test.js
@@ -1,8 +1,58 @@
+let OpendotaApi = require('../src/opendotaApi');
+jest.mock('../src/opendotaApi');
+
 var GameActivityTag = require('../src/GameActivityTag');
+
 describe('GameActivityTag', () => {
   let condition = jest.fn();
   let tag = new GameActivityTag('Test', condition);
   let answerOfLife = {answer: 42};
+
+  describe('static getPredefinedTag(name)', () => {
+    it('returns predefined tag by given name', () => {
+      expect(GameActivityTag.getPredefinedTag('Rusty').name).toBe('Rusty');
+    });
+  });
+
+  describe('tags', () => {
+    describe('Rusty', () => {
+      var rusty = GameActivityTag.getPredefinedTag('Rusty');
+      var fakeStat = {player: {profile: {account_id: 33}}};
+      const mockWinLose = jest.fn();
+
+      beforeEach(() => {
+        OpendotaApi.mockImplementation(() => {
+          return {
+            winLose: mockWinLose.mockResolvedValue({win: 2, lose: 2}),
+          };
+        });
+      });
+
+      it('requests with OpendotaApi', async () => {
+        await rusty.isSatisfied(fakeStat);
+        const mockOpendotaApi = OpendotaApi.mock.instances[0];
+
+        expect(OpendotaApi).toHaveBeenCalledTimes(1);
+        expect(mockWinLose).toHaveBeenCalledTimes(1);
+        expect(mockWinLose).toHaveBeenCalledWith({
+          playerId: fakeStat.player.profile.account_id,
+          date: 14,
+        });
+      });
+
+      it('returns true if played less than 3 games in last 2 weeks', async () => {
+        mockWinLose.mockResolvedValueOnce({win: 1, lose: 1});
+
+        expect(await rusty.isSatisfied(fakeStat)).toBe(true);
+      });
+
+      it('returns false if played more than 3 games in last 2 weeks', async () => {
+        mockWinLose.mockResolvedValueOnce({win: 3, lose: 1});
+
+        expect(await rusty.isSatisfied(fakeStat)).toBe(false);
+      });
+    });
+  });
 
   describe('name', () => {
     it('returns assigned name', () => {
@@ -12,7 +62,9 @@ describe('GameActivityTag', () => {
 
   describe('isSatisfied()', () => {
     it('calls given callback function to identify if is satisfied', () => {
-      tag.isSatisfied(answerOfLife);
+      condition.mockReturnValue(true);
+      var result = tag.isSatisfied(answerOfLife);
+      expect(result).toBe(true);
       expect(condition).toBeCalledWith(answerOfLife);
     });
   });
